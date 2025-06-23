9586b4981856bb94387b32cf72fb09a9f8af40ca
Fredes Virginia <William-Champion@outlook.com>
2025-06-23 20:26:30 -0300

    w

--- src/components/plantillasTypePeriodico/SecondPlantilla.tsx
+++ src/components/plantillasTypePeriodico/SecondPlantilla.tsx
@@ -1,31 +1,95 @@
-
-
 export default function SecondPlantilla() {
   return (
     <div className="my-5 bg-red-400">
-        
-       <div className="flex justify-between  mx-20  border-t-2 border-b-2 h-36">
-         <div className="border-r-2 w-42 flex justify-center items-center"><p className="">PRIMERO</p></div>
-        <div className="flex justify-center items-center"> <p>Segundo primero</p></div>
-        <div className="border-l-2 w-42 flex justify-center items-center"> <p>Terceroo</p></div>
-        
-       </div>
-       <div className="flex mx-20  justify-between border-b-4">
-            <p>PRIMERO</p>
-            <p>SEGUNDO</p>
+      <div className="flex justify-between  mx-20  border-t-2 border-b-2 h-36">
+        <div className="border-r-2 w-42 flex justify-center items-center">
+          <p className="">PRIMERO</p>
         </div>
-        <div className="py-9">
-            <h1 className="text-center text-8xl">Bolas de Algodon</h1>
+        <div className="flex justify-center items-center">
+          {" "}
+          <p>Segundo primero</p>
         </div>
-        <div className="mx-20">
-            <p className="p-funny text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem molestiae tempore pariatur perferendis dicta cupiditate magnam error, quasi repudiandae, obcaecati eum facere, aperiam assumenda quae molestias fuga saepe sequi! Nemo.</p>
+        <div className="border-l-2 w-42 flex justify-center items-center">
+          {" "}
+          <p>Terceroo</p>
         </div>
+      </div>
+      <div className="flex mx-20  justify-between border-b-4">
+        <p>PRIMERO</p>
+        <p>SEGUNDO</p>
+      </div>
+      <div className="py-9">
+        <h1 className="text-center text-8xl">Bolas de Algodon</h1>
+      </div>
+      <div className="mx-20">
+        <p className="p-funny text-center">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+          molestiae tempore pariatur perferendis dicta cupiditate magnam error,
+          quasi repudiandae, obcaecati eum facere, aperiam assumenda quae
+          molestias fuga saepe sequi! Nemo.
+        </p>
+      </div>
 
-        <div className="mx-20 flex mt-5 bg-red-700">
-            <div className="flex-[2] border">primero</div>
-            <div className="flex-[4] border"> segundo</div>
+      <div className="mx-20 flex mt-5 bg-red-700 h-[55rem] gap-10">
+        <div className="  flex flex-col flex-[2] ">
+          <div className="flex-[2] border-y-4 text-wrap p-5 ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,
+            incidunt molestias. Nesciunt quo nobis possimus. Nisi, mollitia sunt
+            voluptas in dicta nobis, magni id, commodi vero aliquam atque
+            explicabo. Voluptas!
+          </div>
+          <div className="flex-[7] p-5">
+ 
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est enim
+              ab provident quas nostrum, ex amet sapiente quasi veritatis a, eum
+              consequatur aut optio iusto, tenetur perferendis eligendi vitae
+              ipsum.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est enim
+              ab provident quas nostrum, ex amet sapiente quasi veritatis a, eum
+              consequatur aut optio iusto, tenetur perferendis eligendi vitae
+              ipsum.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est enim
+              ab provident quas nostrum, ex amet sapiente quasi veritatis a, eum
+              consequatur aut optio iusto, tenetur perferendis eligendi vitae
+             
+            </p>
+             <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est enim
+              ab provident quas nostrum, ex amet sapiente quasi veritatis a, eum
+              consequatur aut optio iusto, tenetur perferendis eligendi vitae
+             
+            </p>
+             <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est enim
+              ab provident quas nostrum, ex amet sapiente quasi veritatis a, eum
+              consequatur aut optio iusto, tenetur perferendis eligendi vitae
+             
+            </p>
+          </div>
         </div>
-        
+        <div className="flex-[5] "> 
+
+          <div className="bg-blue-100 flex justify-center items-center">
+            <div className="h-96 w-96 p-10">
+              <img className="w-full h-full object-cover" src="https://media.istockphoto.com/id/1363288621/es/foto/mujer-trabajando-en-casa-con-una-pc-de-pantalla-en-blanco.jpg?s=612x612&w=0&k=20&c=Y1IyhlD2GtxLEmfDrqNc1DBAG5TcgGwUtfYlj6U_TsY=" alt="" />
+            </div>
+          </div>
+          <div className="flex gap-5">
+            <div className="p-5"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo voluptates voluptate veritatis. Vitae est fuga quaerat, quo earum, mollitia nostrum totam ab expedita nemo tempora ducimus consequuntur incidunt repellat harum!</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo voluptates voluptate veritatis. Vitae est fuga quaerat, quo earum, mollitia nostrum totam ab expedita nemo tempora ducimus consequuntur incidunt repellat harum!</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo voluptates voluptate veritatis. Vitae est fuga quaerat, quo earum, mollitia nostrum totam ab expedita nemo tempora ducimus consequuntur incidunt repellat harum!</p>
+            </div>
+            <div className="p-5"><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum aperiam amet minus doloribus, dolor a fuga mollitia officiis eos, accusamus debitis itaque repellat reprehenderit atque ipsam, ipsum magnam dolore! Id!</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo voluptates voluptate veritatis. Vitae est fuga quaerat, quo earum, mollitia nostrum totam ab expedita nemo tempora ducimus consequuntur incidunt repellat harum!</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }

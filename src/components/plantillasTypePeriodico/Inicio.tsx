e13979e52882535f356dab645fc9abff5f1a9b57
Fredes Virginia <William-Champion@outlook.com>
2025-06-26 01:58:24 -0300

    W

--- src/components/plantillasTypePeriodico/Inicio.tsx
+++ src/components/plantillasTypePeriodico/Inicio.tsx
@@ -1,3 +1,5 @@
+import ME from "../../assets/img/Captura de pantalla (6).png"
+
 export default function Inicio() {
   return (
     <div
@@ -12,7 +14,7 @@ export default function Inicio() {
         style={{
           backgroundColor: "#d5dfe6",
         }}
-        className=" p-3 flex gap-10 justify-center"
+        className=" p-3 flex  justify-between px-20"
       >
         <p>Primero</p>
         <p>Segundo</p>
@@ -21,16 +23,38 @@ export default function Inicio() {
 
       <div className="p-3 flex ">
         <div className="flex-[3] bg-re-400 ">
-          <p className="h1-inicio text-4xl">Bolas de Algodon</p>
+          <p className="h1-inicio md:text-2xl text-4xl">Bolas de Algodon</p>
         </div>
 
-        <div className="flex flex-[8] bg-red800 ml-50  pt-2  gap-25">
+        <div className="flex flex-[8] bg-red800 ml-42  pt-2  gap-25">
           <p>Primero</p>
           <p>Segundo</p>
           <p>Tercero</p>
         </div>
       </div>
       <div className="border-2"></div>
+      <div className="flex justify-center items-center min-h-screen g-blue-400">
+        <div className=" flex-1  p-10 flex flex-col justify-center items-center">
+          <p className="h1-edu text-center">
+            Soy Vicky, también conocida como John Clein. Desarrolladora web.
+          </p>
+          <p className="h1-edu text-center">
+            Me gusta pensar que llevo dentro a un noble ingles rebelde del siglo
+            XVII.
+          </p>
+          <p className="h1-edu text-center">
+            En este blog hablo de desarrollo web, sí… Pero también de las
+            preguntas que no se responden con código: reflexiones, vivencias y
+            poemas que nacen del caos y la calma.
+          </p>
+        </div>
+        <div className=" flex-1 flex justify-center bg-rd-700 ">
+
+           <div className="h-66 w-66">
+        <img src={ME} alt="Vicky" className=" w-full m-auto bg-bue-700 h-auto object-contain rounded-3xl " />
+      </div>
+        </div>
+      </div>
     </div>
   );
 }

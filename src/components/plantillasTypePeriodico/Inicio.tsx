ca1590b793fb94ba910d6b05877f8ee59893a0e3
Jarry1222 <William-Champion@outlook.com>
2025-06-27 17:23:40 -0300

    w

--- src/components/plantillasTypePeriodico/Inicio.tsx
+++ src/components/plantillasTypePeriodico/Inicio.tsx
@@ -1,60 +1,53 @@
-import ME from "../../assets/img/Captura de pantalla (6).png"
-
+import ME from "../../assets/img/Captura_de_pantalla__1_-removebg-preview.png";
+import fondo from "../../assets/img/fondo.jpg"
 export default function Inicio() {
   return (
-    <div
-      className=""
-      style={{
-        backgroundColor: "#fbfbf3",
-      }}
-    >
-      <div></div>
+   <div className="relative w-full min-h-screen overflow-hidden text-white">
+  {/* Imagen de fondo ocupando toda la pantalla */}
+  <div className="absolute inset-0 z-0 flex">
+    <img src={fondo} alt="" className="w-full h-full object-cover" />
+    <img src={fondo} alt="" className="w-full h-full object-cover" />
+    <img src={fondo} alt="" className="w-full h-full object-cover" />
+  </div>
 
-      <div
-        style={{
-          backgroundColor: "#d5dfe6",
-        }}
-        className=" p-3 flex  justify-between px-20"
-      >
-        <p>Primero</p>
-        <p>Segundo</p>
-        <p>Tercero</p>
+  {/* Contenido encima de la imagen */}
+  <div className="relative z-10  ">
+    <div className="w-full pt-5 flex justify-center items-center">
+      <p className="bg-white/50 backdrop-blur-md text-black rounded-xl px-9 py-1 text-center">Primero</p>
+     
+    </div>
+
+    <div className="p-3 flex">
+      <div className="flex-[3] pl-10">
+        <p className="h1-inicio md:text-2xl text-4xl">Bolas de Algodon</p>
+      </div>
+      <div className="flex bg-red-00 justify-between mx-40 flex-[8] ml-10 pt-2 gap-10">
+        <p className="bg-white/50 backdrop-blur-md text-black rounded-xl px-9 py-1">Primero</p>
+      <p className="bg-white/50 backdrop-blur-md text-black rounded-xl px-9 py-1">Primero</p>
+       <p className="bg-white/50 backdrop-blur-md text-black rounded-xl px-9 py-1">Primero</p>
+      </div>
+    </div>
+
+    <div className="border-2 my-5"></div>
+
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="flex-1 p-10 flex flex-col justify-center items-center">
+        <p className="h1-edu text-center">Soy Vicky, también conocida como John Clein. Desarrolladora web.</p>
+        <p className="h1-edu text-center">Me gusta pensar que llevo dentro a un noble inglés rebelde del siglo XVII.</p>
+        <p className="h1-edu text-center">
+          En este blog hablo de desarrollo web, sí… Pero también de las preguntas que no se responden con código:
+          reflexiones, vivencias y poemas que nacen del caos y la calma.
+        </p>
       </div>
 
-      <div className="p-3 flex ">
-        <div className="flex-[3] bg-re-400 ">
-          <p className="h1-inicio md:text-2xl text-4xl">Bolas de Algodon</p>
-        </div>
-
-        <div className="flex flex-[8] bg-red800 ml-42  pt-2  gap-25">
-          <p>Primero</p>
-          <p>Segundo</p>
-          <p>Tercero</p>
-        </div>
-      </div>
-      <div className="border-2"></div>
-      <div className="flex justify-center items-center min-h-screen g-blue-400">
-        <div className=" flex-1  p-10 flex flex-col justify-center items-center">
-          <p className="h1-edu text-center">
-            Soy Vicky, también conocida como John Clein. Desarrolladora web.
-          </p>
-          <p className="h1-edu text-center">
-            Me gusta pensar que llevo dentro a un noble ingles rebelde del siglo
-            XVII.
-          </p>
-          <p className="h1-edu text-center">
-            En este blog hablo de desarrollo web, sí… Pero también de las
-            preguntas que no se responden con código: reflexiones, vivencias y
-            poemas que nacen del caos y la calma.
-          </p>
-        </div>
-        <div className=" flex-1 flex justify-center bg-rd-700 ">
-
-           <div className="h-66 w-66">
-        <img src={ME} alt="Vicky" className=" w-full m-auto bg-bue-700 h-auto object-contain rounded-3xl " />
-      </div>
+      <div className="flex-1 flex justify-center">
+        <div className="w-96 h-96 -translate-x-10 rounded-2xl shadow-lg overflow-hidden">
+          <img src={ME} alt="Vicky" className="w-full h-full object-contain" />
         </div>
       </div>
     </div>
+  </div>
+</div>
+
   );
 }
